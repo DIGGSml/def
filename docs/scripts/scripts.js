c39eaef172ba6b6cc659d5e675f280ef6400aa6c
dponti <dponti@usgs.gov>
2025-01-17 01:12:09 -0800

    Revert "Update script"
    This reverts commit 1e6f6997592d87f4a336eb246d2bb4b043c75557.

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -50,8 +50,8 @@ function highlight_row() {
             
             // Now get cell info to show instance example
             
-            var id = rowSelected.cells[1].innerHTML;
-            var identifier = rowSelected.cells[0].innerHTML;
+            var id = rowSelected.cells[0].innerHTML;
+            var identifier = rowSelected.cells[1].innerHTML;
             var element = document.getElementById("gml:id").innerHTML;
             var url = document.getElementById("url").innerHTML+"&quot;&gt;";
             var txt = 'Example instance:<p style="color: red;">&lt;';
